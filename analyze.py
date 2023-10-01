20a7dcac62b497d36b9c126692dbbd71c7fbc51d
JoonaManninen <joona.manninen7@gmail.com>
2023-10-01 19:12:44 +0300

    All the files

---
+++ analyze.py
@@ -0,0 +1,131 @@
+import pandas as pd
+import json
+import warnings
+
+
+# Function to calculate and update driver averages based on teammates' averages
+def get_new_average(dataframe, driver_averages, num):
+    # Create a new DataFrame to store updated data
+    new_dataframe = pd.DataFrame(
+        columns=["driverid", "driver_name", "result", "average"]
+    )
+
+    # Iterate through each row in the input DataFrame
+    for index, row in dataframe.iterrows():
+        driver_id = row["driverid"]
+        driver_name = row["driver_name"]
+        results_str = row["result"]
+        old_average = float(row["average"])
+
+        result_list = []
+        counter = 0
+        total_time = 0
+
+        if len(results_str) == 2 and check == 0:
+            # Create a new data entry for driver who doesn't have results.
+            new_data = {
+                "driverid": driver_id,
+                "driver_name": driver_name,
+                "result": [],
+                "average": 1.0,
+            }
+        elif len(results_str) == 0:
+            # Create a new data entry for driver who doesn't have results.
+            new_data = {
+                "driverid": driver_id,
+                "driver_name": driver_name,
+                "result": [],
+                "average": 1.0,
+            }
+        else:
+            # Check if 'result' column is a JSON string or already parsed list
+            if check == 0:
+                results = json.loads(results_str)
+            else:
+                results = results_str
+
+            # Iterate through each result item in the results list. This is where driver results are stored and we go through them one by one.
+            for result_item in results:
+                time = result_item["time"]
+                tm_id = result_item["tmId"]
+                # When driver doesn't have teammate
+                if tm_id == -1:
+                    product = time
+                    # Adding the new result to the list.
+                    result_list.append({"time": product, "tmId": tm_id})
+                    total_time = product + total_time
+                    counter += 1
+                # When driver has teammate
+                else:
+                    tm_average = driver_averages.get(tm_id, None)
+                    if tm_average is not None:
+                        # Product is the drivers time multiplied by his teammate average.
+                        product = time * tm_average
+                        # Adding the new result to the list.
+                        result_list.append({"time": product, "tmId": tm_id})
+                        total_time = product + total_time
+                        counter += 1
+            new_average = total_time / counter
+
+            # Update the driver's average
+            if old_average < new_average:
+                average = old_average - abs(pow((1 - new_average), num))
+            else:
+                average = old_average + abs(pow((1 - new_average), num))
+
+            # Create a new data entry for the updated driver
+            new_data = {
+                "driverid": driver_id,
+                "driver_name": driver_name,
+                "result": result_list,
+                "average": average,
+            }
+
+        warnings.simplefilter(action="ignore", category=FutureWarning)
+        # Append the new data to the new DataFrame
+        new_dataframe = new_dataframe.append(new_data, ignore_index=True)
+
+    # Move this line outside of the if condition
+    return new_dataframe
+
+
+# Function to fix JSON-like strings by replacing single quotes with double quotes
+def fix_json_string(json_str):
+    return json_str.replace("'", '"')
+
+
+# Create an empty DataFrame to store the updated results
+new_results_df = pd.DataFrame(columns=["driverid", "driver_name", "result", "average"])
+
+# Read the original results DataFrame and create a dictionary of driver averages
+results_df = pd.read_csv("driver_results.csv")
+driver_averages = {row["driverid"]: row["average"] for _, row in results_df.iterrows()}
+
+check = 0
+results_df["result"] = results_df["result"].apply(fix_json_string)
+counter = 1
+
+# Get the initial updated results DataFrame
+new_results_df = get_new_average(results_df, driver_averages, counter)
+
+print(new_results_df)
+
+check = 1
+
+
+# Iterate the counter and update the results DataFrame
+while counter < 30:
+    counter += 1
+    new_results_df = get_new_average(new_results_df, driver_averages, counter)
+
+
+# Extract the "driver_name" and "average" columns from the final DataFrame
+driver_average_dict = new_results_df[["driver_name", "average"]].to_dict(
+    orient="records"
+)
+
+sorted_data = sorted(driver_average_dict, key=lambda x: x["average"])
+
+# Print each player and their average in separate rows
+for item in sorted_data:
+    print(f"Driver: {item['driver_name']}, Average: {item['average']}")
---
+++ generateData.py
@@ -0,0 +1,287 @@
+import pandas as pd
+import warnings
+
+
+# Function to get the teammates of a driver in a specific race
+def get_teammates(driver_id, race_id):
+    # Filter the result data for the specified driverId and raceId
+    driver_race_data = result_data[
+        (result_data["driverId"] == driver_id) & (result_data["raceId"] == race_id)
+    ]
+
+    if not driver_race_data.empty:
+        # Get the constructorId of the specified driver in the specified race
+        constructor_id = driver_race_data.iloc[0]["constructorId"]
+
+        # Find the teammate(s) with the same raceId and constructorId (excluding the specified driver)
+        teammates = result_data[
+            (result_data["raceId"] == race_id)
+            & (result_data["constructorId"] == constructor_id)
+            & (result_data["driverId"] != driver_id)
+        ]
+
+        # Extract teammate driverIds from the teammates DataFrame
+        teammate_ids = teammates["driverId"].tolist()
+
+        if len(teammate_ids) == 0:
+            return -1
+
+        return teammate_ids[0]
+
+    return None  # No data found for the specified driverId and raceId
+
+
+# Function to get the list of unique raceIds for a specific driver
+def driver_races(driver):
+    # Filter the result data for the specified driverId
+    driver_data = result_data[result_data["driverId"] == driver]
+
+    # Get a list of unique raceIds for the specified driver
+    unique_race_ids = driver_data["raceId"].unique()
+
+    return unique_race_ids
+
+
+# Convert the time column to float representing seconds
+def time_to_seconds(time_str):
+    parts = time_str.split(":")
+    minutes = int(parts[0])
+    seconds = float(parts[1])
+    return minutes * 60 + seconds
+
+
+# Function which changes the data from time column to seconds
+def resulttime_to_seconds(row):
+    time_str = row["time"]
+    race_id = row["raceId"]
+
+    # Drivers time is \N when he was lapped or did not finish
+    if time_str == "\\N":
+        return 0
+    # Only first time in race is formatted like hours:minutes:second.milliseconds and other are formatted like +5.56.
+    if race_id in race_results_dict:
+        if time_str.startswith("+"):
+            time_str = time_str[1:]
+            time_str = time_str.split(":")
+            if len(time_str) == 2:
+                # Format: minutes:seconds.milliseconds
+                seconds_milliseconds = time_str[1].split(".")
+
+                if len(seconds_milliseconds) == 2:
+                    # Format second.milliseconds
+                    minutes = int(time_str[0])
+                    seconds, milliseconds = int(seconds_milliseconds[0]), int(
+                        seconds_milliseconds[1]
+                    )
+                    base_time = race_results_dict.get(race_id)
+                    base_time = int(base_time[0])
+                    total_seconds = (
+                        base_time + (minutes * 60) + seconds + (milliseconds / 1000)
+                    )
+                else:
+                    # Format minutes:second
+                    minutes = int(time_str[0])
+                    seconds = int(time_str[1])
+                    base_time = race_results_dict.get(race_id)
+                    base_time = int(base_time[0])
+                    total_seconds = base_time + (minutes * 60) + seconds
+            else:
+                # Format: seconds.milliseconds
+                time_str = time_str[0]
+                seconds_milliseconds = time_str.split(".")
+                seconds, milliseconds = int(seconds_milliseconds[0]), int(
+                    seconds_milliseconds[1]
+                )
+                base_time = race_results_dict.get(race_id)
+                base_time = int(base_time[0])
+                total_seconds = base_time + seconds + (milliseconds / 1000)
+        else:
+            return 0
+
+    else:
+        # Split the time string by ":"
+        time_parts = time_str.split(":")
+
+        if len(time_parts) == 3:
+            # Format: hours:minutes:seconds.milliseconds
+            seconds_milliseconds = time_parts[2].split(".")
+            hours, minutes = int(time_parts[0]), int(time_parts[1])
+            seconds, milliseconds = int(seconds_milliseconds[0]), int(
+                seconds_milliseconds[1]
+            )
+            total_seconds = (
+                (hours * 3600) + (minutes * 60) + seconds + (milliseconds / 1000)
+            )
+        else:
+            # Format: minutes:seconds.milliseconds
+            seconds_milliseconds = time_parts[1].split(".")
+            minutes = int(time_parts[0])
+            seconds, milliseconds = int(seconds_milliseconds[0]), int(
+                seconds_milliseconds[1]
+            )
+            total_seconds = (minutes * 60) + seconds + (milliseconds / 1000)
+
+        # RaceId doesn't exist, create a new entry with a list containing the base_result_str
+        race_results_dict[race_id] = [total_seconds]
+
+    return total_seconds
+
+
+# Function which makes the comparison between laptimes.
+def compare_times(driver_id, race_id, teammate_id, laptime_raceId):
+    if race_id in laptime_raceId:
+        # Driver time
+        dtime = 0
+        # Teammate time
+        tmtime = 0
+
+        if teammate_id == -1:
+            return 1.0
+        # Getting drivers lap data
+        driver_laps = lap_time_data[
+            (lap_time_data["raceId"] == race_id)
+            & (lap_time_data["driverId"] == driver_id)
+        ]
+        # Getting teammates lap data
+        teammate_laps = lap_time_data[
+            (lap_time_data["raceId"] == race_id)
+            & (lap_time_data["driverId"] == teammate_id)
+        ]
+
+        laps = min(len(driver_laps), len(teammate_laps))
+
+        if laps == 0:
+            return 1.0
+        else:
+            # Iterating through data to get laptimes and adding them up.
+            for i in range(laps):
+                dtime += driver_laps.iloc[i]["time"]
+                tmtime += teammate_laps.iloc[i]["time"]
+
+        result = dtime / tmtime
+
+        return result
+    # Situation when the race doesn't have laptime data
+    else:
+        if teammate_id == -1:
+            return 1.0
+        # Calculate race result times
+        driver_race_data = result_data[
+            (result_data["raceId"] == race_id) & (result_data["driverId"] == driver_id)
+        ]
+        teammate_race_data = result_data[
+            (result_data["raceId"] == race_id)
+            & (result_data["driverId"] == teammate_id)
+        ]
+
+        if driver_race_data.iloc[0]["time"] == 0:
+            return -1
+
+        if (
+            teammate_race_data.iloc[0]["time"] == 0
+            and driver_race_data.iloc[0]["time"] != 0
+        ):
+            return 1.0
+
+        if not driver_race_data.empty:
+            driver_finish_time = driver_race_data.iloc[0]["time"]
+        else:
+            driver_finish_time = None
+
+        if not teammate_race_data.empty:
+            teammate_finish_time = teammate_race_data.iloc[0]["time"]
+        else:
+            teammate_finish_time = None
+
+        if driver_finish_time is not None and teammate_finish_time is not None:
+            result = driver_finish_time / teammate_finish_time
+        else:
+            result = 1.0  # Default result if data is missing
+
+        return result
+
+
+# Function to get the driver's name based on driverId
+def get_driver_name(driver_id):
+    driver_info = driver_data[driver_data["driverId"] == driver_id]
+    if not driver_info.empty:
+        return driver_info.iloc[0]["driverRef"]
+    else:
+        return "Unknown"
+
+
+# Create an empty DataFrame to store results
+results_df = pd.DataFrame(columns=["driverid", "driver_name", "result", "average"])
+
+# Initialize a dictionary to store raceId to base_result_str mappings
+race_results_dict = {}
+
+# Read driver, lap time, and result data from CSV files
+driver_data = pd.read_csv("drivers.csv")
+lap_time_data = pd.read_csv("lap_times.csv")
+result_data = pd.read_csv("results.csv")
+
+# Changing the "time" column data to seconds.
+lap_time_data["time"] = lap_time_data["time"].apply(time_to_seconds)
+result_data["time"] = result_data.apply(resulttime_to_seconds, axis=1)
+
+
+# Get unique driver IDs
+unique_drivers = driver_data["driverId"].unique()
+# All the raceId's that are in the laptime_data, We can use this to check what method we need to use when comparing times
+unique_race_ids = pd.unique(lap_time_data["raceId"])
+
+
+# Loop through each unique driver
+for i in range(len(unique_drivers)):
+    sum = 0
+    counter = 0
+    list = []
+
+    # All raceIds driver has done
+    all_driver_races = driver_races(unique_drivers[i])
+
+    # Loop through each race for the current driver
+    for j in range(len(all_driver_races)):
+        # Getting teammates id
+        teammate_id = get_teammates(unique_drivers[i], all_driver_races[j])
+
+        a = compare_times(
+            unique_drivers[i], all_driver_races[j], teammate_id, unique_race_ids
+        )
+
+        if a == -1:
+            continue
+
+        counter += 1
+        sum = a + sum
+
+        results = {"time": a, "tmId": teammate_id}
+        list.append(results)
+
+    name = get_driver_name(unique_drivers[i])
+
+    # Checking if driver got any values
+    if counter == 0:
+        counter = 1
+        sum = 1.0
+
+    # Calculate the average lap time for the current driver
+    average = sum / counter
+
+    new_data = {
+        "driverid": unique_drivers[i],
+        "driver_name": name,
+        "result": list,
+        "average": average,
+    }
+
+    # Append the new data to the results DataFrame
+    warnings.simplefilter(action="ignore", category=FutureWarning)
+    results_df = results_df.append(new_data, ignore_index=True)
+
+# Save the results DataFrame to a CSV file
+results_df.to_csv("driver_results.csv", index=False)
+
+# Print the results DataFrame
+print(results_df)
